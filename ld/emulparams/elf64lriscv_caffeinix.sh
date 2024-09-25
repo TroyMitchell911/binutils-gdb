c848a4f40e5f6fd1774cf714270f03c75341a7d5
Troy Mitchell <TroyMitchell988@gmail.com>
2024-09-25 10:05:07 +0800

    caffeinix

---
+++ ld/emulparams/elf64lriscv_caffeinix.sh
@@ -0,0 +1,4 @@
+# 引入基础的 RISC-V 64 位设置
+source_sh ${srcdir}/emulparams/elf64lriscv.sh
+
+
---
+++ ld/emulparams/elf_riscv32_caffeinix.sh
@@ -0,0 +1,6 @@
+# 引入基础的 RISC-V 32 位设置
+source_sh ${srcdir}/emulparams/elf32lriscv.sh
+
+# 自定义设置
+TEXT_START_ADDR=0x08000000
+
